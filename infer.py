3cdeb9eb7ca83b6e93033fa40dbc7e10aea1996b
Shihang Hu <its.hushihang@gmail.com>
2023-05-29 00:57:04 -0700

    correct a typo

--- infer.py
+++ infer.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 event = ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
 signature = ['a', 'b', 'a', 'c']
-approximation_parameter = 0
 
 
 def k_equal_appx_match(sig, event, appr_para):
@@ -29,24 +28,35 @@ def k_equal_appx_match(sig, event, appr_para):
         if j == n-1 and C[i][j] == appr_para:
             M = []
             p = i
-            q = n-1
+            q = j
             while q > 0:
                 if C[p][q] == C[p-1][q-1] and event[p] == sig[q]:
+                    M.append(p)
                     p = p-1
                     q = q-1
-                    M.append(p)
                 elif C[p-1][q] < (C[p][q-1]+1):
                     p = p-1
                 else:
                     q = q-1
             L.append(M)
+            print(C)
             for j in range(1, n):
                 C[i][j] = j
                 start = i
-    print(C)
     return L
 
 
+def k_nomore_appx_match(sig, event, appr_para):
+    return 0
+
+
+def act_infer(sig, event, appr_para):
+    return 0
+
+
 if __name__ == '__main__':
-    L = k_equal_appx_match(signature, event, approximation_parameter)
+    k1 = 0
+    k2 = 0
+    k3 = 0
+    L = k_equal_appx_match(signature, event, k1)
     print(L)
