4a98ab9752443747b0d5d0f739e74498a920b973
Shihang Hu <its.hushihang@gmail.com>
2023-06-02 17:37:14 -0700

    add some examples about file IO

--- examples.py
+++ examples.py
@@ -1,6 +1,10 @@
-event = ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_1= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_2= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_3= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_4= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+
 with open("activities_generated.dat",'a') as ag:
-    for i in event:
+    for i in event_1:
         ag.write(i)
     ag.write("\n")
 with open("activities_generated.dat",'+r') as ag:
