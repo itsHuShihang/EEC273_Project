1ded2104d4d2ad0271f55e0d4811d76923e4632d
Shihang Hu <its.hushihang@gmail.com>
2023-06-02 18:21:58 -0700

    fix bug

--- examples.py
+++ examples.py
@@ -1,35 +1,35 @@
 import os
 
-event_1= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
-event_2= ['a', 'c', 'd', 'b', 's']
-event_3= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
-event_4= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_1 = ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_2 = ['a', 'c', 'd', 'b', 's']
+event_3 = ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+event_4 = ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
 
-file_name="activites_generated.dat"
+file_name = "activites_generated.dat"
 if os.path.exists(file_name):
     os.makedirs(file_name)
 
-with open("activities_generated.dat",'a') as ag:
+with open("activities_generated.dat", 'a') as ag:
     for i in event_1:
         ag.write(i)
     ag.write("\n")
     for i in event_2:
         ag.write(i)
     ag.write("\n")
-with open("activities_generated.dat",'+r') as ag:
-    test=ag.readlines()
+with open("activities_generated.dat", '+r') as ag:
+    test = ag.readlines()
 
 print(test)
 
-evve=[[] for i in range(30)] 
+evve = [[] for i in range(30)]
 # TODO: 30 is not enough here, how to ensure the number is enough
 
-i=0
+index = 0
 for str in test:
     for item in str:
-        if item !='\n':
-            evve[i].append(item)
+        if item != '\n':
+            evve[index].append(item)
         if item == '\n':
-            i=i+1
-            i=i%30  # TODO: need a better solution if out of range
+            index = index+1
+            index = index % 30  # TODO: need a better solution if out of range
 print(evve)
