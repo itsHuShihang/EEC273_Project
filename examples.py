093fcdbc47baa4f0b9cadc19938a8d2c5ff1e53d
Shihang Hu <its.hushihang@gmail.com>
2023-06-02 18:18:48 -0700

    add creat file

--- examples.py
+++ examples.py
@@ -1,7 +1,13 @@
+import os
+
 event_1= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
 event_2= ['a', 'c', 'd', 'b', 's']
 event_3= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
 event_4= ['a', 'a', 'b', 'a', 'c', 'a', 'b', 'a', 'a', 'b', 'a', 'b', 'c']
+
+file_name="activites_generated.dat"
+if os.path.exists(file_name):
+    os.makedirs(file_name)
 
 with open("activities_generated.dat",'a') as ag:
     for i in event_1:
